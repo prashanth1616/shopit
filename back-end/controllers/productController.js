9aa4b920af8e8d115ffaad7de044fb5432d8b552
prashanth <prashanth.chinala@gmail.com>
2021-02-25 10:57:38 +0530

    delete product

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -39,3 +39,40 @@ res.status(200).json({
 })
 
 }
+
+// update product
+exports.updateProduct= async (req,res,next)=>{
+    let product= await Product.findById(req.params.id);
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        })
+    }
+    product=await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+exports.deleteProduct= async (req,res,next)=>{
+    let product= await Product.findById(req.params.id);
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        })
+    }
+    product=await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
--- back-end/routes/product.js
+++ back-end/routes/product.js
@@ -1,10 +1,13 @@
 const express=require('express')
 const router =express.Router();
 
-const {getProducts, newProduct, getSingleProduct}=require('../controllers/productController');
+const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct}=require('../controllers/productController');
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
+router.route('/admin/products/new').post(newProduct);
+router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id').delete(deleteProduct);
 
-router.route('/products/new').post(newProduct)
+
 module.exports=router;
